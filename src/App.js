0b08b5ecf2d01885c8d2a111359e1d49d53cd2e5
Sotiris Giannopoulos <sgiannopoulos95@gmail.com>
2022-06-26 20:25:03 +0300

    Added card component and style

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import Card from './components/Card'
+
+export default class App extends Component {
+  render() {
+    return (
+        <Card />
+    )
+  }
+}
---
+++ src/components/Card.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import '../style.css'
+
+export default class Card extends Component {
+  render() {
+    return (
+      <div className='card-container'>
+       Card Title
+      </div>
+    )
+  }
+}
