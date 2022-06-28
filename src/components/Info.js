22bb65ec1e168d573de045d52522e90b63cbbc6e
Sotiris Giannopoulos <sgiannopoulos95@gmail.com>
2022-06-28 13:59:07 +0300

    Added text description.

--- src/components/Info.js
+++ src/components/Info.js
@@ -6,9 +6,27 @@ import SocialsButton from './SocialsButton'
 export default class Info extends Component {
   render() {
     return (
-        <div className='info-panel'>
-          <Title name = {"Stelios Artsontoulis"}/>
-          <SocialsButton />
+        <div className='info-panel flex flex-col'>
+          <Title className='flex flex-row' name = {"Stelios Artsontoulis"}/>
+          <div className='flex flex-row flex-grow-1 justify-between flex-wrap mt-4 ml-auto mr-auto'>
+            <SocialsButton className="flex" selection = {"mail"} />
+            <SocialsButton className="flex" selection = {"linkedin"} />
+          </div>
+          <div className='flex flex-col flex-grow-1  mt-8 ml-2'>
+            <h2 className='flex flex-row text-white'>About</h2>
+            <p className='flex flex-row text-slate-400 text-center'>
+              Dolor veniam cupidatat aliquip eu aute esse in aliquip est. 
+              Pariatur eu adipisicing officia eu aliqua voluptate esse do amet amet enim officia non.
+            </p>
+          </div>
+          <div className='flex flex-col flex-grow-1  mt-1 ml-2 mb-4'>
+            <h2 className='flex flex-row text-white'>Interests</h2>
+            <p className='flex flex-row text-slate-400 text-center'>
+              Dolor veniam cupidatat aliquip eu aute esse in aliquip est. 
+              Pariatur eu adipisicing officia eu aliqua voluptate esse do amet amet enim officia non.
+            </p>
+          </div>
+          
         </div>
     )
   }
--- src/components/SocialsButton.js
+++ src/components/SocialsButton.js
@@ -2,11 +2,28 @@ import React, { Component } from 'react'
 
 export default class SocialsButton extends Component {
   render() {
-    return (
-      <div>
+
+    if (this.props.selection === "mail") {
+      return (
+        <div className=''>
+            <button type='button' className='inline-block px-6 py-2.5 bg-white text-slate-700 font-medium text-xs leading-tight rounded shadow-md hover:bg-slate-700 hover:shadow-lg active:shadow-lg transition duration-150 ease-in-out'> 
+            <i className='fa fa-envelope'></i> Mail </button>
+        </div>
+      )
+    }else if (this.props.selection === "linkedin"){
+      return (
+        <div className=''>
+            <button type='button' className=' inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight rounded shadow-md hover:bg-slate-700 hover:shadow-lg active:shadow-lg transition duration-150 ease-in-out'> 
+            <i className='fa fa-linkedin pr-1'></i> LinkedIn </button>
+        </div>
+      )
+    }else {
+      return (
+        <div>
           <button type='button' className='inline-block px-6 py-2.5 bg-white text-slate-700 font-medium text-xs leading-tight rounded shadow-md hover:bg-slate-700 hover:shadow-lg active:shadow-lg transition duration-150 ease-in-out'> 
-          Mail <i></i></button>
+          </button>
       </div>
-    )
+      )
+    }
   }
 }
